81ca94726e06f739e3be4878b480b16a7708d9ff
Santo Shakil <shakil.mezbah@gmail.com>
2023-08-10 10:05:49 +0600

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use async_std::io;
-use futures::{future::Either, prelude::*, select};
+use futures::{future::Either, prelude::*};
 use libp2p::{
     core::{muxing::StreamMuxerBox, transport::OrTransport, upgrade},
     gossipsub, identity, mdns, noise,
@@ -8,12 +8,12 @@ use libp2p::{
     tcp, yamux, PeerId, Transport,
 };
 use libp2p_quic as quic;
-use std::collections::hash_map::DefaultHasher;
 use std::error::Error;
 use std::hash::{Hash, Hasher};
 use std::time::Duration;
+use std::{collections::hash_map::DefaultHasher, net::SocketAddr};
+use tokio::net::{TcpListener, TcpStream};
 
-// We create a custom network behaviour that combines Gossipsub and Mdns.
 #[derive(NetworkBehaviour)]
 struct MyBehaviour {
     gossipsub: gossipsub::Behaviour,
@@ -22,12 +22,10 @@ struct MyBehaviour {
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // Create a random PeerId
     let id_keys = identity::Keypair::generate_ed25519();
     let local_peer_id = PeerId::from(id_keys.public());
     println!("Local peer id: {local_peer_id}");
 
-    // Set up an encrypted DNS-enabled TCP Transport over the yamux protocol.
     let tcp_transport = tcp::async_io::Transport::new(tcp::Config::default().nodelay(true))
         .upgrade(upgrade::Version::V1Lazy)
         .authenticate(noise::Config::new(&id_keys).expect("signing libp2p-noise static keypair"))
@@ -42,51 +40,55 @@ async fn main() -> Result<(), Box<dyn Error>> {
         })
         .boxed();
 
-    // To content-address message, we can take the hash of message and use it as an ID.
     let message_id_fn = |message: &gossipsub::Message| {
         let mut s = DefaultHasher::new();
         message.data.hash(&mut s);
         gossipsub::MessageId::from(s.finish().to_string())
     };
 
-    // Set a custom gossipsub configuration
     let gossipsub_config = gossipsub::ConfigBuilder::default()
-        .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
-        .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
-        .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
+        .heartbeat_interval(Duration::from_secs(10))
+        .validation_mode(gossipsub::ValidationMode::Strict)
+        .message_id_fn(message_id_fn)
         .build()
         .expect("Valid config");
 
-    // build a gossipsub network behaviour
     let mut gossipsub = gossipsub::Behaviour::new(
         gossipsub::MessageAuthenticity::Signed(id_keys),
         gossipsub_config,
     )
     .expect("Correct configuration");
-    // Create a Gossipsub topic
     let topic = gossipsub::IdentTopic::new("test-net");
-    // subscribes to our topic
     gossipsub.subscribe(&topic)?;
 
-    // Create a Swarm to manage peers and events
     let mut swarm = {
         let mdns = mdns::async_io::Behaviour::new(mdns::Config::default(), local_peer_id)?;
         let behaviour = MyBehaviour { gossipsub, mdns };
         SwarmBuilder::with_async_std_executor(transport, behaviour, local_peer_id).build()
     };
 
-    // Read full lines from stdin
     let mut stdin = io::BufReader::new(io::stdin()).lines().fuse();
 
-    // Listen on all interfaces and whatever port the OS assigns
     swarm.listen_on("/ip4/0.0.0.0/udp/0/quic-v1".parse()?)?;
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
     println!("Enter messages via STDIN and they will be sent to connected peers using Gossipsub");
 
-    // Kick it off
+    let addr = get_addr();
+    let listener = tokio::runtime::Runtime::new()?.block_on(TcpListener::bind(&addr))?;
+    println!("Listening on: {}", addr);
+    // _ = async {
+    //     loop {
+    //         let (stream, addr) = listener.accept().await.unwrap();
+    //         tokio::spawn(handle_connection(stream, addr));
+    //     }
+    // };
+
     loop {
-        select! {
+        tokio::select! {
+            Ok((stream, addr)) = listener.accept() => {
+                tokio::spawn(handle_connection(stream, addr));
+            },
             line = stdin.select_next_some() => {
                 if let Err(e) = swarm
                     .behaviour_mut().gossipsub
@@ -122,4 +124,28 @@ async fn main() -> Result<(), Box<dyn Error>> {
             }
         }
     }
+}
+
+async fn handle_connection(stream: TcpStream, addr: SocketAddr) {
+    println!("Incoming TCP connection from: {}", addr);
+    let stream = tokio_tungstenite::accept_async(stream)
+        .await
+        .expect("Error during the websocket handshake occurred");
+    println!("WebSocket connection established: {}", addr);
+    let (mut write, mut read) = stream.split();
+    while let Some(msg) = read.next().await {
+        let msg = msg.expect("Failed to get msg");
+        println!("Received a message from {}: {}", addr, msg);
+        write.send(msg).await.expect("Failed to send msg");
+    }
+}
+
+pub fn get_addr() -> String {
+    for port in 8080..=8090 {
+        let addr = SocketAddr::from(([127, 0, 0, 1], port));
+        if std::net::TcpListener::bind(addr).is_ok() {
+            return addr.to_string();
+        }
+    }
+    return SocketAddr::from(([127, 0, 0, 1], 8080)).to_string();
 }
